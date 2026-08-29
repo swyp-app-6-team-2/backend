ff47dad0e8cee78294c99c067511830d621f3c9f
Heo-YJ <heoyejin9178@gmail.com>
2026-08-29 19:30:31 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/star_pick/starpick/StarpickApplication.java
@@ -0,0 +1,13 @@
+package com.star_pick.starpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarpickApplication.class, args);
+	}
+
+}
